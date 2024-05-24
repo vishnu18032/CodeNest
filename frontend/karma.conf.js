48daef4028891e335f37fdb2beab71de68f901df
vishnu18032 <112427746+vishnu18032@users.noreply.github.com>
2024-05-24 18:01:00 +0530

    Update karma.conf.js

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -14,12 +14,9 @@ module.exports = function (config) {
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        clearContext: false // leave Jasmine Spec Runner output visible in browser
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
@@ -37,7 +34,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'], // Use Chrome Headless instead of Chrome
     singleRun: false,
     restartOnFileChange: true
   });
